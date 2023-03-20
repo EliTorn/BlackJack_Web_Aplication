ea3bf8c8e6ee866375875fb67e66683c9eb74c62
root1 <eliyahron13@gmail.com>
2023-03-20 02:13:42 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// const submitButton = document.getElementById("submitButton");
+// submitButton.addEventListener("click", function(event) {
+//     event.preventDefault(); // Prevent the form from submitting
+//     const name = document.getElementById("name").value;
+//     // Do something with the name value
+//     console.log("Name submitted: " + name);
+// });
+
+function doSomthing(){
+    let a = document.querySelector("form");
+    let b = a.el.value;
+    alert(b);
+//Document.getElementById("label").innerText = "Your bit was ";
+}
